b6cad9847faa20efd5a28d159946b83f7f5947c6
rahul <rahulbarnwalonlyu2@gmail.com>
2022-04-28 12:33:46 +0530

    changes in ui

--- src/App.js
+++ src/App.js
@@ -20,45 +20,48 @@ function App() {
 
   return (
     <div className="App">
-      <h1>QR Code Generator</h1>
-      <div className="input-box">
-        <div className="gen">
-          <input
-            type="text"
-            onChange={(e) => {
-              setTemp(e.target.value);
-            }}
-            placeholder="Enter text to encode"
-          />
-          <button className="button" onClick={handleClick}>
-            Generate
-          </button>
+      <div className="Vector"></div>
+      <div className="main">
+        <h1>QR Code Generator</h1>
+        <div className="input-box">
+          <div className="gen">
+            <input
+              type="text"
+              onChange={(e) => {
+                setTemp(e.target.value);
+              }}
+              placeholder="Enter text to encode"
+            />
+            <button className="button" onClick={handleClick}>
+              Generate
+            </button>
+          </div>
+          <div className="extra">
+            <h5>Background Color:</h5>
+            <input
+              type="color"
+              onChange={(e) => {
+                setBgColor(e.target.value.substring(1));
+              }}
+            />
+            <h5>Dimension:</h5>
+            <input
+              type="range"
+              min="200"
+              max="600"
+              value={size}
+              onChange={(e) => {
+                setSize(e.target.value);
+              }}
+            />
+          </div>
         </div>
-        <div className="extra">
-          <h5>Background Color:</h5>
-          <input
-            type="color"
-            onChange={(e) => {
-              setBgColor(e.target.value.substring(1));
-            }}
-          />
-          <h5>Dimension:</h5>
-          <input
-            type="range"
-            min="200"
-            max="600"
-            value={size}
-            onChange={(e) => {
-              setSize(e.target.value);
-            }}
-          />
+        <div className="output-box">
+          <img src={qrCode} alt="" />
+          <a href={qrCode} download="QRCODE">
+            <button type="button">Download</button>
+          </a>
         </div>
-      </div>
-      <div className="output-box">
-        <img src={qrCode} alt="" />
-        <a href={qrCode} download="QRCODE">
-          <button type="button">Download</button>
-        </a>
       </div>
     </div>
   );
